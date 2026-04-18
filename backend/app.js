8f59c03285f22995cf56c3075a7408365251f83a
Nikita Sharma <nikita@example.com>
2026-04-18 19:54:08 +0530

    remove node_modules from repo

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ const authRouter= require("./router/authRouter");
 const itemRouter= require("./router/itemRouter")
 
 const app= express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 app.use(express.urlencoded());
 
